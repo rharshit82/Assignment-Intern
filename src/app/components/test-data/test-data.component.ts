7eca56a444f30c3c225cc9e8dec63ca450992dfa
harshit <Harshit.foodvybe@outlook.com>
2022-11-19 00:13:43 +0530

    UI Improved v2

--- src/app/components/test-data/test-data.component.ts
+++ src/app/components/test-data/test-data.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import data from './data.json'
+
+function convertFromMili (x:number): string{
+  let milliseconds: number = Math.trunc((x%1000)/100)
+  let seconds: number = Math.trunc((x/1000)%60)
+  let minutes: number = Math.trunc((x/(1000*60))%60)
+  let hours: number = Math.trunc((x/(1000*60*60))%24)
+
+  let hourstring:string  = (hours < 10) ? "0" + hours.toString() : hours.toString();
+  let minutestring:string  = (minutes < 10) ? "0" + minutes.toString() : minutes.toString();
+  let secondstring:string = (seconds < 10) ? "0" + seconds.toString() : seconds.toString();
+
+  return hourstring + ":" + minutestring + ":" + secondstring + "." + milliseconds;
+}
 @Component({
   selector: 'app-test-data',
   templateUrl: './test-data.component.html',
   styleUrls: ['./test-data.component.scss']
 })
+ 
 export class TestDataComponent implements OnInit {
   lis = [];
   constructor(private http : HttpClient) { 
     
   }
+ 
 
   ngOnInit(): void {
     this.lis = data;
     // this.lis = this.lis.slice(0, 10)
+    this.lis.forEach((ele) =>{
+      ele.RunTime = convertFromMili(ele.RunTime)
+      ele.Results[0].RunTime = convertFromMili(ele.Results[0].RunTime)
+      ele.Results[1].RunTime = convertFromMili(ele.Results[1].RunTime)
+    })
     console.log(this.lis)
   }
-
+  
 }
